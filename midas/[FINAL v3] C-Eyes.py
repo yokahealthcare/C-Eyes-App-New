5c4cc7ca715f502244675afd13fe06d6a30b1fa7
Erwin Yonata <86058013+yokahealthcare@users.noreply.github.com>
2023-09-01 17:37:04 +0700

    Update [FINAL v3] C-Eyes.py

--- midas/[FINAL v3] C-Eyes.py
+++ midas/[FINAL v3] C-Eyes.py
@@ -200,7 +200,8 @@ class ImageController:
                 # draw the rectangle on the face
                 cv2.rectangle(self.frame, (x, y), (x + w, y + h), GREEN, 2)
                 # Centimeter Text
-                cv2.putText(self.frame, f"Person {idx + 1}: {round(self.distance[idx], 2)} CM", (30, 35 + idx * 20), fonts, 0.5, GREEN, 1)
+                cv2.putText(self.frame, f"Person {idx + 1}: {round(self.distance[idx], 2)} CM", (30, 35 + idx * 20),
+                            fonts, 0.5, GREEN, 1)
 
         elif t == "depthmap":
             if self.faces is None or self.mean_area is None:
@@ -212,7 +213,8 @@ class ImageController:
                 # draw the rectangle on the face
                 cv2.rectangle(self.frame, (x, y), (x + w, y + h), GREEN, 2)
                 # Depth Map Text
-                cv2.putText(self.frame, f"Person {idx + 1}: {round(self.mean_area[idx], 3)}", (x, y + h), fonts, 0.5, BLACK, 1)
+                cv2.putText(self.frame, f"Person {idx + 1}: {round(self.mean_area[idx], 3)}", (x, y + h), fonts, 0.5,
+                            BLACK, 1)
 
     def show(self, title="Showing Image"):
         cv2.imshow(title, self.frame)
@@ -527,14 +529,13 @@ if __name__ == "__main__":
             # Print the list of files
             print(f"Select {len(files)} MiDAS pre-trained model below:")
             for idx, file in enumerate(files):
-                print(f"{idx+1}. {file}")
+                print(f"{idx + 1}. {file}")
 
             choice = int(input("Enter your choice: "))
-            midas(files[choice-1])
+            midas(files[choice - 1])
         elif choice == "3":
             print("Exiting...")
             print("Thank You")
             break
         else:
             print("Invalid choice. Please select 1, 2, or 3.")
-
